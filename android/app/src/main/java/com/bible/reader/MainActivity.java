02b8d0e3716b33f9822dc82d9f08321e71ec33ec
zhao-zg <787854091@qq.com>
2026-07-02 00:23:07 +0800

    重构

--- android/app/src/main/java/com/bible/reader/MainActivity.java
+++ android/app/src/main/java/com/bible/reader/MainActivity.java
@@ -5,6 +5,9 @@ import com.getcapacitor.BridgeActivity;
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(android.os.Bundle savedInstanceState) {
+        // 重要：必须在 super.onCreate() 之前注册自定义插件
+        registerPlugin(ApkInstallerPlugin.class);
+
         // 提前绑定 TTS 引擎，省去 2-3 秒初始化延迟
         TTSForegroundService.prewarmTts(this);
         super.onCreate(savedInstanceState);
